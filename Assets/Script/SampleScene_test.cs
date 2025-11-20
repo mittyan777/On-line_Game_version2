ae3549c26c28daefaf386e674cae5f0a450d72af
mittyan777 <131222874+mittyan777@users.noreply.github.com>
2025-11-20 15:30:13 +0900

    Title（仮）作成

---
+++ Assets/Script/SampleScene_test.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class SampleScene_test : MonoBehaviourPunCallbacks
+{
+    private int playerCount;
+    const int maxPlayers = 3;
+    public string[] playerPrefabName;
+    public Transform[] spawnPoints;
+
+    public string selectedRoomName = "Room1"; // デフォルトは Room1
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // UI ボタンから呼び出す
+    public void JoinRoom1() => TryJoinRoom("Room1");
+    public void JoinRoom2() => TryJoinRoom("Room2");
+    public void JoinRoom3() => TryJoinRoom("Room3");
+
+    private void TryJoinRoom(string roomName)
+    {
+        selectedRoomName = roomName;
+        RoomOptions options = new RoomOptions { MaxPlayers = maxPlayers };
+        PhotonNetwork.JoinOrCreateRoom(roomName, options, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        playerCount = PhotonNetwork.PlayerList.Length;
+
+        // 🔍 現在のルーム情報をデバッグ出力
+        Debug.Log($"現在のルーム名: {PhotonNetwork.CurrentRoom.Name}");
+        Debug.Log($"現在のプレイヤー数: {playerCount}");
+        Debug.Log($"最大プレイヤー数: {PhotonNetwork.CurrentRoom.MaxPlayers}");
+
+        if (playerCount <= maxPlayers)
+        {
+            PhotonNetwork.Instantiate(
+                playerPrefabName[playerCount - 1],
+                spawnPoints[playerCount - 1].position,
+                Quaternion.identity
+            );
+        }
+    }
+
+}
---
+++ Assets/Title_control.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_control : MonoBehaviour
+{
+    [SerializeField] GameObject camera;
+    bool star_trigger;
+    float Speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fogDensity = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (star_trigger == false)
+        {
+            if (camera.transform.position.z >= 0)
+            {
+                camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, -60);
+            }
+
+        }
+        else
+        {
+            if (RenderSettings.fogDensity > 0f && RenderSettings.fogDensity < 1f)
+            {
+                RenderSettings.fogDensity += 0.001f;
+            }
+        }
+        if (camera.transform.position.z <= 37)
+        {
+            camera.transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene("lobby");
+        }
+
+    }
+    public void Gamestar()
+    {
+        if (star_trigger == false)
+        {
+            star_trigger = true;
+            camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, 0);
+        }
+    }
+}
