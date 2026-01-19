27db9cce42af8784f0630c321bf427a9a65d1a0e
mittyan777 <C3289@oic.jp>
2026-01-19 21:36:09 +0900

    修正

--- Assets/Clear_manager.cs
+++ Assets/Clear_manager.cs
@@ -24,6 +24,9 @@ public class Clear_manager : MonoBehaviourPunCallbacks
     AudioScript audioScript;
 
     public AudioClip BGM_File;
+
+    bool p1;
+    bool p2;
     // Start is called before the first frame update
     void Start()
     {
@@ -49,36 +52,36 @@ public class Clear_manager : MonoBehaviourPunCallbacks
     
 
 
-        if (MainGameManager.Player_GameObject_name[0] == "Player1(Clone)")
+        if (MainGameManager.Player_GameObject_name[0] == "Player1(Clone)" && p1 == true)
         {
             p1_obj[0].SetActive(true);
         }
-        if (MainGameManager.Player_GameObject_name[0] == "Player2(Clone)")
+        if (MainGameManager.Player_GameObject_name[0] == "Player2(Clone)" && p1 == true)
         {
             p1_obj[1].SetActive(true);
         }
-        if (MainGameManager.Player_GameObject_name[0] == "Player3(Clone)")
+        if (MainGameManager.Player_GameObject_name[0] == "Player3(Clone)" && p1 == true)
         {
             p1_obj[2].SetActive(true);
         }
-        if (MainGameManager.Player_GameObject_name[1] == "Player1(Clone)")
+        if (MainGameManager.Player_GameObject_name[1] == "Player1(Clone)" && p2 == true)
         {
             p2_obj[0].SetActive(true);
         }
-        if (MainGameManager.Player_GameObject_name[1] == "Player2(Clone)")
+        if (MainGameManager.Player_GameObject_name[1] == "Player2(Clone)" && p2 == true)
         {
             p2_obj[1].SetActive(true);
         }
-        if (MainGameManager.Player_GameObject_name[1] == "Player3(Clone)")
+        if (MainGameManager.Player_GameObject_name[1] == "Player3(Clone)" && p2 == true)
         {
             p2_obj[2].SetActive(true);
         }
 
         // プレイヤー1が脱出したか
-        bool p1 = MainGameManager.Game_completer_name[0] == "Player";
+         p1 = MainGameManager.Game_completer_name[0] == "Player";
 
         // プレイヤー2が脱出したか
-        bool p2 = MainGameManager.Game_completer_name[1] == "Player2";
+         p2 = MainGameManager.Game_completer_name[1] == "Player2";
 
         // 結果表示
         Game_completer_mesh[0].text = p1 ? "脱出" : "脱出失敗";
