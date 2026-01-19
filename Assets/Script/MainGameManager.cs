85f7da7734468b377d5ea526fe2853eab1eb50ab
mittyan777 <C3289@oic.jp>
2026-01-19 15:24:06 +0900

    修正

--- Assets/Script/MainGameManager.cs
+++ Assets/Script/MainGameManager.cs
@@ -420,7 +420,8 @@ public class MainGameManager : MonoBehaviourPunCallbacks
             isLeaving = true;
             if (PhotonNetwork.IsMasterClient)
             {
-                PhotonNetwork.LoadLevel("Game_Clear");
+                Invoke(nameof(Game_Clear_Scene), 4);
+              
             }
             photonView.RPC(nameof(Clear_load), RpcTarget.All);
         }
@@ -436,7 +437,10 @@ public class MainGameManager : MonoBehaviourPunCallbacks
         // }
 
     }
-
+    public void Game_Clear_Scene()
+    {
+        PhotonNetwork.LoadLevel("Game_Clear");
+    }
     public void jail_doa_control()
     {
         DoorController door = jail_doa.GetComponent<DoorController>();
